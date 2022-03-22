dc1a2b2f68eae3b13ceb53d05ae92d20cdd3b982
fullth <lth129@naver.com>
2022-03-22 23:37:08 +0900

    first commit

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const mysql = require('mysql');
+const dbconfig   = require('../config/database');
+const connection = mysql.createConnection(dbconfig);
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  connection.query('SELECT * from Users', (error, rows) => {
+    if (error) throw error;
+    
+    console.log('User info is: ', rows);
+    res.send(rows);
+  });
+});
+
+module.exports = router;
