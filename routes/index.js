c9613d8eb0b98ffe9fd1c27ae14b4046c032bffd
fullth <lth129@naver.com>
2022-03-23 02:33:48 +0900

    🙌 프로젝트 세팅

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.cookie('tester', {
+    id: 'test',
+    name: '테스트쿠키',
+    authorized: true
+  })
+
   res.render('index', { title: 'Express' });
 });
 
